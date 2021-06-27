a27854bb00a555a3321e11183ee9fc9fb311b269
lassaadbaati <lassaad.baati@gmail.com>
2021-06-27 05:06:05 +0200

    Add files via upload

---
+++ src/main/java/com/lassaad/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.lassaad;
+public class HelloWorld {
+  public static void main(String[] args) {
+    HelloWorld myWorld = new HelloWorld();
+    String said = myWorld.sayHelloTo("World");
+    System.out.println(said);
+    }
+  public String sayHelloTo(String to) {
+    return "salam "+to+"!";
+    }
+}
